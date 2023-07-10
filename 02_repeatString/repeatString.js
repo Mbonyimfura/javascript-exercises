ccb5d8311472efa07b5c9396fb9c8728ba14d39d
Mbonyimfura <mbonyimfuratresor@gmail.com>
2023-07-10 16:15:20 +0200

    Run tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str,n) {
+let repeatString='';
+    for(let i=0;i<=str.length-1;i++){
+repeatString+=str;
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
