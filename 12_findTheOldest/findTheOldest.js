c9322cfb8cf570c568da45c6f58d6b6b7993aaae
Mbonyimfura <mbonyimfuratresor@gmail.com>
2023-07-21 12:28:16 +0200

    Pass the tet cases

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+function findTheOldest(people) {
+  const today = new Date().getFullYear();
+  const oldestPerson = people.reduce((oldest, person) => {
+    const currentAge = person.yearOfDeath
+      ? person.yearOfDeath - person.yearOfBirth
+      : today - person.yearOfBirth;
+    const oldestAge = oldest.yearOfDeath
+      ? oldest.yearOfDeath - oldest.yearOfBirth
+      : today - oldest.yearOfBirth;
+    return currentAge > oldestAge ? person : oldest;
+  });
 
-};
+  return oldestPerson;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
