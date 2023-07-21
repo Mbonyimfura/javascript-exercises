31b61af77c6c70fba1e047588dfd31835d67964a
Mbonyimfura <mbonyimfuratresor@gmail.com>
2023-07-21 11:22:53 +0200

    Pass all cases

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(number) {
+    if(number<0) return 'OOPS'
+    let a=0,b=1, c;
+    for(let i=2;i<=number;i++){
+      c=a+b;
+      a=b;
+      b=c
+    }
+    return b
+    };
 
 // Do not edit below this line
 module.exports = fibonacci;
